b636574d670e94da739d45e746ee107c16fa2a07
antasofa7 <arfiantasofa@gmail.com>
2022-02-12 15:17:28 +0700

    slicing desain to html

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 0) {
+        $('.navbar').addClass('sticky');
+        $('.btn-menu-color').addClass('unvisible');
+        $('.btn-menu-white').removeClass('unvisible');
+    } else {
+        $('.navbar').removeClass('sticky');
+        $('.btn-menu-white').addClass('unvisible');
+        $('.btn-menu-color').removeClass('unvisible');
+    }
+});
+
+$('.toggle-menu').click(function(e) {
+    e.preventDefault();
+    $('.menu').toggleClass('active');
+});
+
+$('.nav-link').click(function(e) {
+    e.preventDefault();
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+    $('.menu').removeClass('active');
+})
+
+const swiperHeader = new Swiper('.headerSwiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+        },
+    },  
+ });
+
+
+ const swiperProjects = new Swiper('.projectSwiper', {
+    // Optional parameters
+    loop: true,
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+      },
+  });
